c5425ffd2af3ba0abfd599220d2c4f30aebdfff7
Afzallovessana <125810400+Afzallovessana@users.noreply.github.com>
2023-04-04 14:34:13 +0530

    integrated post and get api

--- src/Pages/AddBook.js
+++ src/Pages/AddBook.js
@@ -1,10 +1,14 @@
 import { TextField } from '@mui/material';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import React,{useState} from 'react';
 import Header from '../Components/Header';
 import { Button } from '@mui/material'
 
 
 const AddBook = () => {
+const navigate=useNavigate();
+
 const [formData, setFormData] = useState({});
 console.log('formData' ,formData);
 
@@ -15,11 +19,21 @@ setFormData((prevData)=> ({
 [name]:value
   }))
 }
+
+const handleSubmit = async (event) =>  {
+  event.preventDefault();
+
+  axios.post("http://localhost:8000/save-book-data",formData).then((res)=>{
+    if(res){navigate('/view-book');
+  }
+  }).catch(error => {console.log("error" , error);})
+}
+
   return (
     <div>
       <Header/>
   <div className='Addbook_Back'><h1>Add Your Book</h1>
-  <form>
+  <form onSubmit={handleSubmit}>
 <TextField sx={{width:'80%' , margin:'10px'}}label="Book Id" type={"text"} onChange={handleChange} name="book_id" variant='outlined'/>
 <TextField sx={{width:'80%' , margin:'10px'}}label="Book Name" type={"text"} onChange={handleChange} name="book_name" variant='outlined'/>
 <TextField sx={{width:'80%' , margin:'10px'}}label="Book Description" type={"text"} onChange={handleChange} name="book_description" variant='outlined'/>
--- src/Pages/AddBook/ViewBook/ViewBook.js
+++ src/Pages/AddBook/ViewBook/ViewBook.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React , {useEffect , useState}  from 'react';
+import Header from '../../../Components/Header';
+import BookCard from'../../../Components/BookCard';
+import axios from 'axios';
+
 
 const ViewBook = () => {
+  const [data , setData] =useState([]);
+  console.log('data=>=>' ,data);
+  const viewAllBookks = async () => {
+    await axios.get('http://localhost:8000/save-book-data').then((res) => {
+    if(res){setData(res.data);
+    
+    }
+    
+    })
+    
+    } 
+    
+    // useEffecr Hook takes two arguments as parameter first is callback function and second is dependency array. 
+    // if dependency array is empty then it is in mounting phses.
+    useEffect(() => {
+    viewAllBookks();
+    
+    },[])
+    
+
+
   return (
     <div>
-      <h1>Sana</h1>
+      <Header/>
+      <BookCard/>
     </div>
   )
 }
